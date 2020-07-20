ebd84e420a3e17bbdcc6b5b7c99246577fd9c130
YuraKolodiy13 <timber2017@gmail.com>
2020-07-20 19:24:45 +0300

    comment app use

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/', createProxyMiddleware({ target: REDIRECTION_URL,
-  logProvider,
-  changeOrigin: true }));
+// app.use('/', createProxyMiddleware({ target: REDIRECTION_URL, changeOrigin: true }));
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
@@ -19,17 +17,3 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
-
-// verbose replacement
-function logProvider(provider) {
-  const logger = new (require('winston').Logger)();
-
-  const myCustomProvider = {
-    log: logger.log,
-    debug: logger.debug,
-    info: logger.info,
-    warn: logger.warn,
-    error: logger.error,
-  };
-  return myCustomProvider;
-}
